ac297950a66d7613d13293f8f265caca2a3dc417
saaas9944-creator <saaas9944@gmail.com>
2026-06-27 15:47:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+var CACHE = 'sms-v1';
+var FILES = ['./sms_analyzer.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
+  );
+});
